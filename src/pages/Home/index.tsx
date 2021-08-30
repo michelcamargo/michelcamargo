1ad41882c2beb9e809e66f3eabd3510804198582
Michel Camargo <w.michelcamargo@gmail.com>
2021-08-29 23:44:47 -0300

    ajustes layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,29 +27,26 @@ function Home(){
                         <Image src={path_ProfilePicture} width="200px"></Image>
                     </Picturebox>
                     <Textbox>
-                        <Textbox.MainTitle color="var(--black)">Michel Camargo</Textbox.MainTitle>
-                        <Textbox.Title color="var(--black)">Estudante, desenvolvedor & UX-UI designer</Textbox.Title>
-                        {/* <H3 color="var(--white)">Desenvolvedor & designer adaptivo e meticuloso</H3> */}
-                        <Textbox.Subtitle color="var(--black)">Meu principal objetivo é oferecer belos produtos com experiência agradável e intuitiva ao usuário.</Textbox.Subtitle>
+                        <Textbox.MainTitle color="var(--text-base-primary)">Michel Camargo</Textbox.MainTitle>
+                        <Textbox.Title color="var(--text-base-primary)">Estudante, desenvolvedor & UX-UI designer</Textbox.Title>
+                        <Textbox.Subtitle color="var(--text-base-primary)">Vamo levantar esse site.</Textbox.Subtitle>
                     </Textbox>
                 </Resume>
 
             </FlexLayoutRow>
 
-            <FlexLayoutRow isFullWidth={true} containerWidth={containerWidth}>
+            <FlexLayoutRow isFullWidth={false} containerWidth={containerWidth}>
 
-                {/* <Separator color="#000000" width="500px"/> */}
-                //     {/* <IntButton2 width='150px' to='/contato' bgColor='var(--white)' color='var(--darkGreen)'>Contato</IntButton2> */}
-                //     <ExtButton2 width='150px' to='https://www.linkedin.com/in/michelscamargo/' bgColor='var(--white)' color='var(--darkGreen)' target="_blank">LinkedIn</ExtButton2>
+                <ExtButton2 width='150px' to='https://www.linkedin.com/in/michelscamargo/' bgColor='var(--white)' color='var(--darkGreen)' target="_blank">LinkedIn</ExtButton2>
 
                 <Skills>
-                    <Card title="Web Development" textColor="var(--white)" bgColor="var(--otherDark)" srcImage={path_SkillJS} >
+                    <Card title="Web Development" textColor="var(--text-base-secondary)" bgColor="var(--background-primary)" srcImage={path_SkillJS} >
                         <Paragraph>
                             Implementação e suporte a aplicações multiplataforma utilizando JavaScript e React.js ao lado de Node.js, Java ou Python.
                             Além disso estudo a implementação de objetos e elementos tipados com TypeScript.
                         </Paragraph>
                     </Card>
-                    <Card title="UI & UX Design" textColor="var(--white)" bgColor="var(--otherDark)" srcImage={path_SkillIHC} >
+                    <Card title="UI & UX Design" textColor="var(--text-base-secondary)" bgColor="var(--background-primary)" srcImage={path_SkillIHC} >
                         <Paragraph>
                             Manter uma experiência positiva ao usuário possibilita o mapeamento de pontos focais e a priorização de recursos, garantindo a alavancagem
                             natural do produto expandindo a ótica de solução e definição dos objetivos.
