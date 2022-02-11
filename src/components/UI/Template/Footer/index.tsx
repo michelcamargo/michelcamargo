1d82e56d4145c9ed5af9d265b679234ae543bf04
mscamargo <w.michelcamargo@gmail.com>
2022-02-11 10:45:47 -0300

    Changelog:   - grade de exibição de portifolio, input em footer

--- src/components/UI/Template/Footer/index.tsx
+++ src/components/UI/Template/Footer/index.tsx
@@ -7,13 +7,19 @@ export default (): JSX.Element => {
         <div className={"footer__wrapper"}>
             <div className={"footer__container container"}>
                 <div className={"footer__menuContainer"}>
-                    <div className={"footer__menuContent"}>
-                        <h6 className={"footer__menuHeading"}>Primeiro menu</h6>
-                        <ul className={"footer__menuList"}>
-                            <li className={"footer__menuItem"}>Item 1</li>
-                            <li className={"footer__menuItem"}>Item 2</li>
-                            <li className={"footer__menuItem"}>Item 3</li>
-                        </ul>
+                    <div>
+                        <div>
+                            <h4>Chama!</h4>
+                            <h5>Say hello!</h5>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                name="footer-contact-form"
+                                placeholder="Seu melhor email"
+                                className={"footer__contactFormInput footer__input input"}
+                            />
+                        </div>
                     </div>
 
                     <div className={"footer__menuContent"}>
--- src/views/Home/components/Portfolio/index.tsx
+++ src/views/Home/components/Portfolio/index.tsx
@@ -15,48 +15,33 @@ export default ({}: PortfolioProps): JSX.Element => {
             <div className={"portfolio__container"}>
                 <div className={"portfolio__caseInformation"}>
                     <h5 className={"portfolio__caseTitle"}>Villa Mercato</h5>
-                    <p className={"portfolio__caseDescription"}>Case ecommerce mercado em VTEX.io</p>
+                    <p className={"portfolio__caseDescription"}>Case ecommerce supermercado em VTEX.io</p>
                 </div>
                 <div className={"portfolio__caseImagesContainer"}>
-                    <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
+                    <div className={"portfolio__caseContainerMobile"}>
+                        <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
+                    </div>
+                    <div className={"portfolio__caseContainerDesktop"}>
+                        <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
+                        <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
+                    </div>
                 </div>
             </div>
 
             <div className={"portfolio__container"}>
-                <div className={"portfolio__caseImagesContainer"}>
-                    <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
-                </div>
                 <div className={"portfolio__caseInformation"}>
                     <h5 className={"portfolio__caseTitle"}>BeBrasil</h5>
-                    <p className={"portfolio__caseDescription"}>Ecommerce em Angular em VTEX API</p>
+                    <p className={"portfolio__caseDescription"}>Ecommerce em Angular usando VTEX API</p>
                 </div>
-            </div>
 
-            <div className={"portfolio__container"}>
-                <div className={"portfolio__caseInformation"}>
-                    <h5 className={"portfolio__caseTitle"}>Ótica Prigol</h5>
-                    <p className={"portfolio__caseDescription"}>Ótica de medianeira, ecommerce VTEX.io</p>
-                </div>
                 <div className={"portfolio__caseImagesContainer"}>
-                    <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
-                </div>
-            </div>
-
-            <div className={"portfolio__container"}>
-                <div className={"portfolio__caseInformation"}>
-                    <h5 className={"portfolio__caseTitle"}>Heading</h5>
-                    <p className={"portfolio__caseDescription"}>teste</p>
-                </div>
-                <div className={"portfolio__caseImagesContainer"}>
-                    <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
-                    <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
+                    <div className={"portfolio__caseContainerMobile"}>
+                        <img className={"portfolio__casePreviewImage"} src={portfolio0} alt={"Preview"} />
+                    </div>
+                    <div className={"portfolio__caseContainerDesktop"}>
+                        <img className={"portfolio__casePreviewImage"} src={portfolio1} alt={"Preview"} />
+                        <img className={"portfolio__casePreviewImage"} src={portfolio2} alt={"Preview"} />
+                    </div>
                 </div>
             </div>
         </div>
