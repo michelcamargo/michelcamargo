b89c09254b20ed013d198a8ea3455208533fc65e
mscamargo <michelcamargo@outlook.com>
2022-05-26 00:13:34 -0300

    changelog:   linting

--- src/app/app-modules/view-template/private/view-template.module.ts
+++ src/app/app-modules/view-template/private/view-template.module.ts
@@ -10,12 +10,12 @@ import { WorkViewComponent } from "../shared/components/work-view/work-view.comp
 import { AboutViewComponent } from "../shared/components/about-view/about-view.component";
 
 import { TemplateService } from "../shared/services/template.service";
-import {ContactComponent} from "../../customers/shared/components/contact/contact.component";
-import {FormsModule} from "@angular/forms";
-import {AuthenticationModule} from "../../authentication/private/authentication/authentication.module";
-import {CustomerModule} from "../../customers/private/customer.module";
-import {PortfolioModule} from "../../portfolio/private/portfolio/portfolio.module";
-import {HeroModule} from "../../hero/private/hero.module";
+import { ContactComponent} from "../../customers/shared/components/contact/contact.component";
+import { FormsModule } from "@angular/forms";
+import { AuthenticationModule } from "../../authentication/private/authentication/authentication.module";
+import { CustomerModule } from "../../customers/private/customer.module";
+import { PortfolioModule } from "../../portfolio/private/portfolio/portfolio.module";
+import { HeroModule } from "../../hero/private/hero.module";
 
 
 @NgModule({
@@ -30,14 +30,14 @@ import {HeroModule} from "../../hero/private/hero.module";
     // misc components
     ContactComponent
   ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        AuthenticationModule,
-        CustomerModule,
-        PortfolioModule,
-        HeroModule
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AuthenticationModule,
+    CustomerModule,
+    PortfolioModule,
+    HeroModule
+  ],
   exports: [
     HeaderComponent,
     FooterComponent,
