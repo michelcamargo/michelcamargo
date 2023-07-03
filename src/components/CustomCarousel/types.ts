3223d59adfff5c163099fea7864933444c22f8e3
mscamargo <michelcamargo@outlook.com>
2023-07-03 14:05:05 -0300

    Preparação build
    Changelog:
      * ~resolução de problemas eslint build

--- src/components/CustomCarousel/types.ts
+++ src/components/CustomCarousel/types.ts
@@ -1,13 +1,9 @@
 import { Swiper } from "swiper";
 
-type CarouselConfig = {
+export type CarouselConfig = {
   spaceBetween: number,
   slidesPerView: number,
   onSlideChange?: (event: Event) => never,
   onSwiper?: (swiper: Swiper) => never,
   onSelect?: (event: Event) => never,
 }
-
-export {
-  CarouselConfig,
-};
