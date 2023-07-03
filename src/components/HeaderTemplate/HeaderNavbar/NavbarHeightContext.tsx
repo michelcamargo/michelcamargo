4e695f2d9317b49f83ae971cf38e24801934783f
mscamargo <michelcamargo@outlook.com>
2023-07-03 14:35:08 -0300

    Preparação build
    Changelog:
      * ~resolução de problemas eslint build

--- src/components/HeaderTemplate/HeaderNavbar/NavbarHeightContext.tsx
+++ src/components/HeaderTemplate/HeaderNavbar/NavbarHeightContext.tsx
@@ -1,27 +1,27 @@
-import { createContext, ReactNode, useMemo, useState } from 'react';
-
-interface Props {
-  children: ReactNode,
-}
-
-const NavbarHeightContext = createContext({
-  navbarHeight: 0,
-  setNavbarHeight: _ => _,
-});
-
-export const NavbarHeightContextProvider = ({ children }: Props) => {
-  const [navbarHeight, setNavbarHeight] = useState(0);
-  
-  const value = useMemo(() => ({
-    navbarHeight,
-    setNavbarHeight,
-  }), [navbarHeight]);
-  
-  return (
-    <NavbarHeightContext.Provider value={value}>
-      {children}
-    </NavbarHeightContext.Provider>
-  );
-};
-
-export default NavbarHeightContext;
+// import { createContext, ReactNode, useMemo, useState } from 'react';
+//
+// interface Props {
+//   children: ReactNode,
+// }
+//
+// const NavbarHeightContext = createContext({
+//   navbarHeight: 0,
+//   setNavbarHeight: _ => _,
+// });
+//
+// export const NavbarHeightContextProvider = ({ children }: Props) => {
+//   const [navbarHeight, setNavbarHeight] = useState(0);
+//
+//   const value = useMemo(() => ({
+//     navbarHeight,
+//     setNavbarHeight,
+//   }), [navbarHeight]);
+//
+//   return (
+//     <NavbarHeightContext.Provider value={value}>
+//       {children}
+//     </NavbarHeightContext.Provider>
+//   );
+// };
+//
+// export default NavbarHeightContext;
