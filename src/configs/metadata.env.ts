1e5969a9970ed24d099ba615b9d678a370212070
mscamargo <michelcamargo@outlook.com>
2023-07-23 23:18:19 -0300

    Ajustes para deploy
    Changelog:
      * Tratamento de erros em fetch server-side content;

--- src/configs/metadata.env.ts
+++ src/configs/metadata.env.ts
@@ -1,4 +1,5 @@
 const MetaConfig = {
+  APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
   VIEW_NAME: 'Michel Camargo',
   VIEW_DESCRIPTION: 'Portfolio',
   VIEW_KEYWORDS: 'portfolio presentation professional',
