70cc3269712a1f164aa00d1cd8f8da5a86107b16
mscamargo <michelcamargo@outlook.com>
2023-07-23 23:32:02 -0300

    Ajustes para deploy
    Changelog:
      * Tratamento de erros em fetch server-side content;

--- src/pages/api/content/index.ts
+++ src/pages/api/content/index.ts
@@ -1,17 +1,21 @@
 import staticHomeContent from '@/domains/HomePage/static';
-import { ServerViewProps } from "@/lib/datahooks";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ServerViewProps>
+  res: NextApiResponse
 ) {
   const queryString = req.query;
   
   if (req.method === 'GET') {
     console.log('GET CONTENT GERAL', queryString);
     
-    res.status(200).json(staticHomeContent);
+    try {
+      res.status(200).json(staticHomeContent);
+    } catch (error) {
+      console.error('Error in server-side fetch:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
   
 }
