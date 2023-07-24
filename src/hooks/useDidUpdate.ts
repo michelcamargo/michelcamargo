008c6eaab5f26bb7c285483ef9d1a555fe800912
mscamargo <michelcamargo@outlook.com>
2023-07-23 22:51:36 -0300

    Ajustes para lint
    Changelog:
      * Tipagens e correções eslint;

--- src/hooks/useDidUpdate.ts
+++ src/hooks/useDidUpdate.ts
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
  * @param {function} callback
  * @param {Array} inputs
  */
-const useDidUpdate = (callback, inputs) => {
+const useDidUpdate = (callback: () => void, inputs: any[]) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(callback, inputs);
 };
