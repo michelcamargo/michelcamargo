a2adb01dd0b573cf8b61e0f4b26299eb7b2f8f87
mscamargo <michelcamargo@outlook.com>
2023-07-23 22:54:44 -0300

    Ajustes para lint
    Changelog:
      * Tipagens e correções eslint;

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
-import { Html, Main, NextScript } from 'next/document';
-import Head from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
-      <body style={{ background: 'black' }}>
+      <body>
         <Main />
         <NextScript />
       </body>
