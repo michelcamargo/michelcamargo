eb176b45d4fd253d60819fb5aa36e743eeaf4009
mscamargo <michelcamargo@outlook.com>
2024-08-15 00:33:10 -0300

    correções componente consentimento p/ deploy

--- src/components/CookiesPopup/styles.ts
+++ src/components/CookiesPopup/styles.ts
@@ -1,3 +1,4 @@
+import UnicodeBg from "@/assets/img/disclaimer/unicodebg.jpg";
 import { Box, styled, Typography } from "@mui/material";
 
 interface StyledProps {
@@ -23,6 +24,43 @@ const DisclaimerText = styled(Typography)<StyledProps>`
   font-size: 12px;
   width: fit-content;
 `;
+
+export const generalStyle = {
+  display: 'block',
+  right: 0,
+  height: 'auto',
+  backgroundColor: 'transparent',
+  backgroundImage: `url(${UnicodeBg.src})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center',
+  color: '#FFFFFF',
+  width: '512px',
+  marginLeft: 'auto',
+  padding: '16px',
+};
+
+export const contentStyle = {
+  display: 'block',
+  padding: 0,
+  height: 'auto',
+  flex: 1,
+};
+
+export const acceptBtnStyle = {
+  borderRadius: '4px',
+  background: 'none',
+  color: 'white',
+  fontWeight: 'bold',
+  padding: 0,
+  textDecoration: 'underline white'
+};
+
+export const refuseBtnStyle = {
+  background: 'black',
+  borderRadius: '4px',
+  fontWeight: 'bold',
+};
 
 export default {
   DisclaimerText,
