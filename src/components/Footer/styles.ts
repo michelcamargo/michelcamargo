94a9628d0077be858327cb07ccc48a043eb2b53a
mscamargo <michelcamargo@outlook.com>
2024-08-14 23:20:32 -0300

    Remoção da master antiga para refatoração

--- src/components/Footer/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-interface FooterProps {}
-
-export const FooterWrapper = styled.footer<FooterProps>``;
-
-export const FooterContainer = styled.footer<FooterProps>``;
