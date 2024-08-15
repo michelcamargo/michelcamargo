6866cf3161a306b9007b10f1a1518fd8eeacb8f7
mscamargo <michelcamargo@outlook.com>
2024-08-15 02:00:06 -0300

    Ajustes para deploy
    Changelog:
      * Correções linting;

--- src/components/SocialPresentation/index.tsx
+++ src/components/SocialPresentation/index.tsx
@@ -12,20 +12,20 @@ interface Props {
 
 const SocialPresentation = ({ socialData }: Props) => {
 	const socialItems = socialData ? socialData.getChildren() : [];
-  
+ 
 	if (!socialData) {
 		return (
 			<p>NO SOCIAL DATA</p>
 		);
 	}
-  
+ 
 	return (
 		<Styled.SocialList>
 			{socialItems?.map((item, index) => {
-				const label = item.getContent('label') as string || '';
-				const href = item.getContent('link') as string || '#';
-				const icon = item.getContent('icon') as string || null;
-				const target = item.getContent('target') as HTMLAttributeAnchorTarget || '_self';
+				const label = item.getValue('label') as string || '';
+				const href = item.getValue('link') as string || '#';
+				const icon = item.getValue('icon') as string || null;
+				const target = item.getValue('target') as HTMLAttributeAnchorTarget || '_self';
 
 				const logEvent = (socialItem: CustomContent) => {
 					return socialItem;
